0684697c62be6ffc3398b7d1781fe0ac73c31e9e
doxingxin <58932492+doxingxin@users.noreply.github.com>
2021-03-29 15:20:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,16 @@
 #
 
 # 使用源码自带ShadowSocksR Plus+出国软件
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom  #透明主题
+git clone https://github.com/garypang13/luci-app-dnsfilter.git package/lean/luci-app-dnsfilter #广告过滤插件
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
+git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
+svn co https://github.com/liuran001/openwrt-packages/luci-app-dockerman package/lean/luci-app-dockerman #dockerman
+svn co https://github.com/liuran001/openwrt-packages/luci-app-iptvhelper package/lean/luci-app-iptvhelper #IPTV
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】

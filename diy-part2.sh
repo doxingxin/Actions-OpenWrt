bd16fd04ed45d6db0d7fa6c76d9d8d7f99651a26
doxingxin <58932492+doxingxin@users.noreply.github.com>
2021-05-20 09:05:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,3 +37,4 @@ sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/
 sed -i 's/"Web 管理"/"Web"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
 sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
+sed -i 's/"VPN"/"OpenVPN客户端"/g' feeds/luci/applications/luci-app-openvpn/po/zh-cn/openvpn.po
